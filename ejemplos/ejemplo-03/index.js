4501a67e3b44dfc8ec66f7cb94a42c8c0d5f6a36
Sebastián Vicencio <sivicencio@gmail.com>
2021-05-18 16:13:52 -0400

    add example 03 - Jest in Node project

---
+++ ejemplos/ejemplo-03/index.js
@@ -0,0 +1,20 @@
+const { add } = require('./src/add');
+const TodoList = require('./src/todo');
+
+console.log('add function')
+console.log(`2 + 3 = ${add(2, 3)}`);
+
+console.log('\n*********************\n');
+
+console.log('TodoList class')
+const todoList = new TodoList(['hola', 'chao']);
+
+todoList.remember(4);
+todoList.prioritize(10);
+console.log('\nPrint list\n');
+console.log(todoList.show());
+
+todoList.getTask();
+
+console.log('\nPrint list\n');
+console.log(todoList.show());
